82568fb08cb76508e1b4a238156d714d8384e255
hmdnalfttah <105841108323@student.unismuh.ac.id>
2025-08-12 16:31:01 +0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,20 @@
+//  import '@/app/ui/global.css'
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts'; // Impor font
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +22,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* Terapkan font ke body */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
